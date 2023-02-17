3c84efd3b18ba15db47451113f55b421dc849c83
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-02-17 10:15:17 -0500

    adds initial HTML skeleton, some beginning scratch of pseudo-code JS

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// DOM Dependencies
+var submitBtn = document.querySelector(".submit-btn");
+// var recentSearches = document.querySelectorAll(".recentSearches");
+var recentSearchesList = document.querySelector('.recent-searches')
+var localStorageArr = []; //create an array to pull all localStorage values and print to LIs.
+
+// Data
+// function to grab values from lcoalstorage to print
+for (i = 0; i < ) {}
+var searchLi = document.createElement("li")
+var liEl = searchLi.textContent(localStorage.getItem(i))
+
+//  Function
+
+// User Interactions
